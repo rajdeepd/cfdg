b78dcd03a1506ab3be5d5c33d3b7771a118d94d3
Kunal Bhatia <kunalb@weboniselab.com>
2012-12-03 19:35:35 +0530

    add carrier wave gem

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,40 +1,41 @@
 class Event < ActiveRecord::Base
   stampable
-  acts_as_soft_deletable   
+  acts_as_soft_deletable
   has_many :event_members
   has_many :users , :through => :event_members
   has_many :comments, :as => :commentable
   has_many :posts
   belongs_to :chapter
   belongs_to :user , :foreign_key => :created_by
-  attr_accessible :title, :event_start_date, :event_end_date, :status, :description, :venue, :entry_fee, :chapter_id , :location,  :address_line1,   :address_line2 ,:event_start_time ,:event_end_time, :city_name, :postal_code, :state_name, :country_name ,:agenda_and_speakers
+  attr_accessible :title, :event_start_date, :event_end_date, :status, :description, :venue, :entry_fee, :chapter_id , :location,  :address_line1,   :address_line2 ,:event_start_time ,:event_end_time, :city_name, :postal_code, :state_name, :country_name ,:agenda_and_speakers,:image
+  mount_uploader :image, ImageUploader
   validates :title,:event_start_time ,:event_start_date, :event_end_date, :event_end_time ,:presence => true
-  
+
 
   validates :event_start_date ,:format => {
-  :with => /^([0-3])?[0-9]\/[0-1][0-9]\/[1-9][0-9][0-9][0-9]$/,
-    :message => "Date should be in dd/mm/yyyy format"
+      :with => /^([0-3])?[0-9]\/[0-1][0-9]\/[1-9][0-9][0-9][0-9]$/,
+      :message => "Date should be in dd/mm/yyyy format"
   } , :unless => Proc.new{|event| event.event_start_date.blank?}
 
   validates :event_end_date, :format => {
-  :with => /^([0-3])?[0-9]\/[0-1][0-9]\/[1-9][0-9][0-9][0-9]$/,
-    :message => "Date should be in dd/mm/yyyy format"
+      :with => /^([0-3])?[0-9]\/[0-1][0-9]\/[1-9][0-9][0-9][0-9]$/,
+      :message => "Date should be in dd/mm/yyyy format"
   } , :unless => Proc.new{|event| event.event_end_date.blank?}
-  
+
 
 
   def <=> (other)
     if (other.event_start_date.blank? or  other.event_start_time.blank?)
       return 1
     elsif (self.event_start_date.blank? or  self.event_start_time.blank?)
-      return 0 
+      return 0
     end
 
-  	Time.parse(other.event_start_date + " " + other.event_start_time) <=> Time.parse(self.event_start_date + " " + self.event_start_time)
-  end 
+    Time.parse(other.event_start_date + " " + other.event_start_time) <=> Time.parse(self.event_start_date + " " + self.event_start_time)
+  end
 
   def am_i_member?(user_id)
-  	self.event_members.where(:user_id => user_id).present?
+    self.event_members.where(:user_id => user_id).present?
   end
 
   def can_i_delete?(user_id, chapter_id)
