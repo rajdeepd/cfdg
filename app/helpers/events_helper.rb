1e13518f1d25b4ae893d7b246ced6ad14753b2d5
Kunal Bhatia <kunalb@weboniselab.com>
2012-12-13 15:41:47 +0530

    define method for event address and also fix issue relationg agenda

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,11 @@
 module EventsHelper
-  #def get_event_address(event)
-  #   return("")
-  #end
+  def get_event_address(event)
+    #return "#{event.venue} #{event.address_line1} #{event.address_line2} #{event.city_name} #{event.postal_code}"
+    address = "#{event.venue}"
+    address =  address +"," + event.address_line1 + "," if event.address_line1.present?
+    address = address + event.address_line2 + "," if event.address_line2.present?
+    address = address + event.city_name + "," if event.city_name.present?
+    address = address + event.postal_code
+    return address
+  end
 end
