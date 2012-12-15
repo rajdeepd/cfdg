aa5f08fdcf1c72f3c691fb90c97d158e5b5dc91b
Aditya <adityagarg.work@gmail.com>
2012-12-15 14:45:14 +0530

    scope for country and state

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -20,7 +20,7 @@ class Chapter < ActiveRecord::Base
   #validations
   validates  :country_id , :state_id, :city_id,:country_name, :state_name, :city_name,:chapter_type, presence: true
   validates :locality, :address ,:landmark, presence: true , :if => lambda { |o| o.chapter_type == "student"}
-  validates :city_name, :uniqueness => true, :if => lambda { |o| o.chapter_type == "professional"}
+  validates_uniqueness_of :city_name, :scope => [:state_name , :country_name], :if => lambda { |o| o.chapter_type == "professional"}
   validates_with CityNameValidator
   #Scopes
    scope :applied_chapters, where(:chapter_status => [:applied, :incubated,:denied])
