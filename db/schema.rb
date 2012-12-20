bf6986b2633df4175daed4ee9be75ca85e65b234
Aditya <adityagarg.work@gmail.com>
2012-12-20 19:51:10 +0530

    change in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20121219054528) do
-
+ActiveRecord::Schema.define(:version => 20121220074128) do
 
   create_table "announcements", :force => true do |t|
     t.string   "title"
@@ -107,7 +105,6 @@ ActiveRecord::Schema.define(:version => 20121219054528) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
