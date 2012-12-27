f45c745a3123ad26805d77df5a5af91a7b9203b4
Kunal Bhatia <kunalb@weboniselab.com>
2012-12-27 14:58:41 +0530

    creating new controller confirmation controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController  < Devise::RegistrationsController 
     before_filter :authenticate_user!, :only => :token
+    #before_filter :login_required, :except=>[:new, :create]
 
     def new
       super      
