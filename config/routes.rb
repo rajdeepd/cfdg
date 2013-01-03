11b89bb57f2ec4234f793e4be8b7b0441ee184ce
Kunal Bhatia <kunalb@weboniselab.com>
2013-01-03 11:05:59 +0530

    created show page for announcements

--- config/routes.rb
+++ config/routes.rb
@@ -112,6 +112,10 @@ CloudfoundryUsergroups::Application.routes.draw do
       end
     end
    end
+
+  resources :announcements do
+
+  end
    
   # The priority is based upon order of creation:
   # first created -> highest priority.
