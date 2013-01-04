d62c332b7556581b2b3520e18462eff10dd3b0ec
Aditya <adityagarg.work@gmail.com>
2013-01-04 12:37:14 +0530

    change in migration file

--- db/migrate/20130103141303_populate_countries_state_city.rb
+++ db/migrate/20130103141303_populate_countries_state_city.rb
@@ -1,9 +1,9 @@
 class PopulateCountriesStateCity < ActiveRecord::Migration
 
   def change
-    `mysql -uroot -pwebonise6186 cf_usergroups < #{Rails.root.join("countries.sql")}`
-    `mysql -uroot -pwebonise6186 cf_usergroups < #{Rails.root.join("states.sql")}`
-    `mysql -uroot -pwebonise6186 cf_usergroups < #{Rails.root.join("cities.sql")}`
+    `mysql -uroot -proot cf_usergroups_production < #{Rails.root.join("countries.sql")}`
+    `mysql -uroot -proot cf_usergroups_production < #{Rails.root.join("states.sql")}`
+    `mysql -uroot -proot cf_usergroups_production < #{Rails.root.join("cities.sql")}`
   end
 
 end
