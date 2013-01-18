f0d58cba848eafea24e2883cc3c5dc8959c98b57
Larry Zhao <thehiddendepth@gmail.com>
2013-01-18 11:59:09 +0800

    qq connect

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,10 @@ class OmniauthCallbacksController < ApplicationController
     handle_callback
   end
 
+  def qq_connect
+    handle_callback
+  end
+
   def handle_callback
     logger.info("*** oauth data *****")
     logger.info(env['omniauth.auth'].inspect)
