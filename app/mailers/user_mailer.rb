8b50f3b8ba3841ce2987a1120639750f5cb1cd92
Larry Zhao <thehiddendepth@gmail.com>
2013-01-22 16:18:49 +0800

    add mail template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,14 +2,15 @@ class UserMailer < ActionMailer::Base
   default :from => "contact.cfdg@gmail.com"
 
   def welcome_mail(user)
-    #logger.info "########## inside welcome mail method ###############"
-    #logger.info "##########this is the tester mailer ###############"
     @user = user
     mail(:to => @user.email, :subject => "Welcome mail")
   end
 
   def confirmation_mail(user)
     @user = user
-    mail(:to => @user.email, :subject => I18n.t("mail.confirmation.subject"))
+
+    mail(:to => @user.email, :subject => I18n.t("mail.subjects.confirmation")) do |format|
+      format.html { render :layout => 'mail_default' }
+    end
   end
 end
