1ef3396b2ff393a4ccc9200b9881d4ffe192bb90
Aditya <adityagarg.work@gmail.com>
2013-02-07 12:39:13 +0530

    whole system jquery recheck

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery
+//= require jquery.remotipart
 //= require_tree .
-
 
 $(function(){
 
@@ -100,14 +99,13 @@ $(function(){
 
   $('#comment_content').die('keypress').live('keypress', function(e){
       if(e.keyCode == 13){
-          e.preventDefault();
       $(this).parents('#new_comment').submit();
          
     }         
 
   });
 
- $('.comment').die('submit').live('submit', function(e){
+ $('.comment').die('submit').live('submit', function(e){    
    if(e.preventDefault) {
       e.preventDefault();
     }
@@ -138,4 +136,3 @@ function hideFlashMessages() {
 function showFlashMessages() {
   $('.alert').slideDown(200);
 }
-
