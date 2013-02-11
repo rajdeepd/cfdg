d2c0a574b8d4392c09257b441c2d6c73347c1660
Aditya <adityagarg.work@gmail.com>
2013-02-11 10:55:33 +0530

    removed cache buster filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,11 +33,11 @@ class ApplicationController < ActionController::Base
     options.merge!({ :locale => I18n.locale })
   end
 
-  def set_cache_buster
-    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
-  end
+  #def set_cache_buster
+  #  response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+  #  response.headers["Pragma"] = "no-cache"
+  #  response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  #end
 
   private
 
