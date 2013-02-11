e4da5a7b253148689fe643977bf56106d9ba3b12
Aditya <adityagarg.work@gmail.com>
2013-02-11 12:49:46 +0530

    comment box clear, comment alignment and event start date format

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,8 +1,12 @@
 module EventsHelper
   def get_event_address(event)
     #return "#{event.venue} #{event.address_line1} #{event.address_line2} #{event.city_name} #{event.postal_code}"
+    if event.venue.present?
+      address = "#{event.venue},"
+    else
     address = "#{event.venue}"
-    address =  address +"," + event.address_line1 + "," if event.address_line1.present?
+    end
+    address =  address + event.address_line1 + "," if event.address_line1.present?
     address = address + event.address_line2 + "," if event.address_line2.present?
     address = address + event.city_name + "," if event.city_name.present?
     address = address + event.postal_code
