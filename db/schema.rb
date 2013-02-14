e8cc61aa0001c6b6b60803f4f8a62eedbbe23174
Aditya <adityagarg.work@gmail.com>
2013-02-14 17:37:09 +0530

    ckeditor options reduction

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130212115639) do
+ActiveRecord::Schema.define(:version => 20130214094732) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(:version => 20130212115639) do
   add_index "activities", ["recipient_id", "recipient_type"], :name => "index_activities_on_recipient_id_and_recipient_type"
   add_index "activities", ["trackable_id", "trackable_type"], :name => "index_activities_on_trackable_id_and_trackable_type"
 
+  create_table "agendas", :force => true do |t|
+    t.string   "description"
+    t.integer  "event_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "announcements", :force => true do |t|
     t.string   "title"
     t.text     "body"
@@ -41,6 +48,7 @@ ActiveRecord::Schema.define(:version => 20130212115639) do
     t.datetime "updated_at",         :null => false
     t.integer  "user_id"
     t.datetime "deleted_at"
+    t.string   "image"
   end
 
   create_table "chapter_members", :force => true do |t|
@@ -78,15 +86,10 @@ ActiveRecord::Schema.define(:version => 20130212115639) do
   end
 
   create_table "cities", :force => true do |t|
-    t.string   "name"
-    t.integer  "state_id"
-    t.integer  "created_by"
-    t.integer  "updated_by"
-    t.datetime "deleted_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "country_id"
-    t.string   "details"
+    t.string  "name"
+    t.integer "country_id"
+    t.integer "state_id"
+    t.string  "details"
   end
 
   create_table "ckeditor_assets", :force => true do |t|
@@ -117,12 +120,7 @@ ActiveRecord::Schema.define(:version => 20130212115639) do
   end
 
   create_table "countries", :force => true do |t|
-    t.string   "name"
-    t.integer  "created_by"
-    t.integer  "updated_by"
-    t.datetime "deleted_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string "name"
   end
 
   create_table "delayed_jobs", :force => true do |t|
@@ -165,6 +163,7 @@ ActiveRecord::Schema.define(:version => 20130212115639) do
     t.datetime "deleted_at"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "status"
   end
 
   create_table "eventbrite_oauth_tokens", :force => true do |t|
@@ -260,13 +259,8 @@ ActiveRecord::Schema.define(:version => 20130212115639) do
   end
 
   create_table "states", :force => true do |t|
-    t.string   "name"
-    t.integer  "country_id"
-    t.integer  "created_by"
-    t.integer  "updated_by"
-    t.datetime "deleted_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer "country_id"
+    t.string  "name"
   end
 
   create_table "users", :force => true do |t|
