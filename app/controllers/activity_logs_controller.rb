7a38837fa6f08f69f96b98cc09f42fa9b25e71a7
Aditya <adityagarg.work@gmail.com>
2013-02-18 13:53:07 +0530

    removed config.js

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -2,7 +2,7 @@ class ActivityLogsController < ApplicationController
   # notifications_controller.rb
 
   def index
-    @activities = PublicActivity::Activity.page(params[:page] || 1).order("created_at DESC").per(3)
+    @activities = PublicActivity::Activity.page(params[:page] || 1).order("created_at DESC").per(20)
     render :layout=>false
   end
 
