df5e3af48a8049c26ab1ba02e2e69954e5169a4b
Aditya <adityagarg.work@gmail.com>
2013-02-19 17:45:40 +0530

    omniauth authentication for fb, gmail

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.logger.progname = "omniauth"
 
   provider :facebook, "543305615699970", "5eeeadf96ed1c5dca0583fa11ad4e77f"
-  provider :twitter, "XDoB4a1h0MOhsvJX6EieQQ", "MBdijWKkZ2pnGvvyjG80p5Vymb9IE08BFvu6FdFbno"
+  #provider :twitter, "XDoB4a1h0MOhsvJX6EieQQ", "MBdijWKkZ2pnGvvyjG80p5Vymb9IE08BFvu6FdFbno"
   #provider :linkedin, "8l32hbmh7udx", "XsBya4XE3gFCp9Gw"
-
+  provider :google, "94772855995.apps.googleusercontent.com", "lcslMhoyxjTZJq78ujSiPgEe"
 end
