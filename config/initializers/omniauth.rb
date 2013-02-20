ce1f993f187a9ddba48313b65c03c28fc151aa9d
Aditya <adityagarg.work@gmail.com>
2013-02-20 19:00:46 +0530

    change of app secret and key for yahoo in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,5 +7,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, "XDoB4a1h0MOhsvJX6EieQQ", "MBdijWKkZ2pnGvvyjG80p5Vymb9IE08BFvu6FdFbno"
   provider :linkedIn, "8l32hbmh7udx", "XsBya4XE3gFCp9Gw"
   provider :google, "94772855995.apps.googleusercontent.com", "lcslMhoyxjTZJq78ujSiPgEe"
-  provider :yahoo, "dj0yJmk9cjNZS1hDSWUxMnVGJmQ9WVdrOWNHNTZhVVZ2TnpZbWNHbzlNVGMxTWpJNU16ZzJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD01YQ--","32157a8754acfca3da5ae9cdc167e840321098fb"
+  # for testing purpose cfdg-testing.cloudfoundry.com
+  provider :yahoo, "dj0yJmk9ckloY2V2eEJVRVY5JmQ9WVdrOVVsVTVVa2x4TldjbWNHbzlPVEk1TURVM01UWXkmcz1jb25zdW1lcnNlY3JldCZ4PTVj","6d2cac7368f4246889a54512f2cee95006d44d0f"
+
+  # for local use local.cfdg.com
+  #provider :yahoo, "dj0yJmk9cjNZS1hDSWUxMnVGJmQ9WVdrOWNHNTZhVVZ2TnpZbWNHbzlNVGMxTWpJNU16ZzJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD01YQ--","32157a8754acfca3da5ae9cdc167e840321098fb"
 end
