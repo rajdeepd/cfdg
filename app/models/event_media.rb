2c14cb6ae98500b4385af12dfbd76e46d5ac5838
sandeep <sandeep@sandeep.(none)>
2013-02-22 14:33:37 +0530

    updates for youtube with popup

--- app/models/event_media.rb
+++ app/models/event_media.rb
@@ -1,11 +1,8 @@
 class EventMedia < ActiveRecord::Base
 
   attr_accessible :url, :category, :event_id
-
   CATEGORIES = ["Video", "Slide"]
-
   validates :url, :presence=> true
-
   scope :videos, where(:category=>"Video")
 
 end
