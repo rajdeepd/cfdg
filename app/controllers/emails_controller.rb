2307466fbeb0d23564965b6bbacd3bdd18e027f4
Aditya <adityagarg.work@gmail.com>
2013-03-01 18:06:21 +0530

    admin css for sending email and showing error messages

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -5,9 +5,14 @@ class EmailsController < ApplicationController
   end
 
   def create
-   logger.info "###################{params.inspect}"
-   #UserMailer.send_custom_mail(params[:to],params[:subject],params[:body]).deliver
-   redirect_to new_email_path, :notice => "Email sent successfully"
+    logger.info "###################{params.inspect}"
+    if UserMailer.send_custom_mail(params[:to],params[:subject],params[:body]).deliver
+      flash[:notice] = "Email sent successfully"
+      redirect_to new_email_path
+    else
+      flash[:custom_error] = "Operation failed, please try again"
+      redirect_to new_email_path
+    end
   end
 
 
