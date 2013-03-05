91dd6c96db1b5d08434ea1a62e187f17ce47d497
Aditya <adityagarg.work@gmail.com>
2013-03-05 11:26:15 +0530

    added space in event media model

--- app/models/event_media.rb
+++ app/models/event_media.rb
@@ -2,7 +2,7 @@ class EventMedia < ActiveRecord::Base
 
   attr_accessible :url, :category, :event_id
   CATEGORIES = ["Video", "Slide"]
-  validates :url, :presence=> true
+  validates :url, :presence => true
   belongs_to :event
   scope :videos, where(:category=>"Video")
 
